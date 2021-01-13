0f199531e91749b543841619f117e8a168639c9c
amomvga <amom_augusto@hotmail.com>
2021-01-13 14:44:00 -0300

    melhoria

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,64 +4,21 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        methods c = new methods();
+        while (true) {
 
-        int option = 5;
-        int num1;
-        int num2;
+            System.out.println("--Choose an option--");
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
+            System.out.println();
+            System.out.println("1 - Sum");
+            System.out.println("2 - Subtraction");
+            System.out.println("3 - Multiplication");
+            System.out.println("4 - Division");
+            System.out.println("0 - Exit");
+            System.out.print("Operation: ");
 
-        System.out.println("--Choose an option--");
-        System.out.println();
-        System.out.println("1 - Sum");
-        System.out.println("2 - Subtraction");
-        System.out.println("3 - Multiplication");
-        System.out.println("4 - Division");
-        System.out.println("0 - Exit");
-        System.out.print("Operation: ");
 
-        option = sc.nextInt();
-
-        if (option == 1) {
-            Scanner sc1 = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            num1 = sc1.nextInt();
-            System.out.print("Enter the second number: ");
-            num2 = sc1.nextInt();
-            int operation = c.sum(num1, num2);
-            System.out.print("The result of the sum is: " + operation);
-
+            new methods().calc(sc.nextInt());
         }
-        else if (option == 2) {
-            Scanner sc1 = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            num1 = sc1.nextInt();
-            System.out.print("Enter the second number: ");
-            num2 = sc1.nextInt();
-            int operation = c.sub(num1, num2);
-            System.out.print("The result of the subtraction is: " + operation);
-        }
-        else if (option == 3) {
-            Scanner sc1 = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            num1 = sc1.nextInt();
-            System.out.print("Enter the second number: ");
-            num2 = sc1.nextInt();
-            int operation = c.mult(num1, num2);
-            System.out.print("The result of the multiplication is: " + operation);
-        }
-        else if (option == 4) {
-            Scanner sc1 = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            num1 = sc1.nextInt();
-            System.out.print("Enter the second number: ");
-            num2 = sc1.nextInt();
-            int operation = c.div(num1, num2);
-            System.out.print("The result of the division is: " + operation);
-        }
-        else
-            System.exit(0);
-
     }
 }
--- src/main/java/entities/methods.java
+++ src/main/java/entities/methods.java
@@ -1,15 +1,55 @@
 package entities;
 
+import java.util.Scanner;
+
 public class methods {
+
+    public void calc(int option) {
+        int num1 = 0;
+        int num2 = 0;
+        int operation = 0;
+
+        if (option != 0) {
+            Scanner sc1 = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            num1 = sc1.nextInt();
+            System.out.print("Enter the second number: ");
+            num2 = sc1.nextInt();
+            operation = 0;
+
+        }
+        switch (option) {
+            case 1:
+
+                operation = sum(num1, num2);
+                break;
+            case 2:
+                operation = sub(num1, num2);
+                break;
+            case 3:
+                operation = mult(num1, num2);
+                break;
+            case 4:
+                operation = div(num1, num2);
+                break;
+            default:
+                System.exit(0);
+        }
+        System.out.print("The result of the sum is: " + operation + "\n");
+    }
+
     public int sum(int num1, int num2) {
         return num1 + num2;
     }
+
     public int sub(int num1, int num2) {
         return num1 - num2;
     }
+
     public int div(int num1, int num2) {
         return num1 / num2;
     }
+
     public int mult(int num1, int num2) {
         return num1 * num2;
     }
